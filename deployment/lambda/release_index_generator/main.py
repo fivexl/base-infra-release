03aac5aeb64f4edc10a409d623bab5d7789393bc
Vladimir Samoylov <cageyv.dev@gmail.com>
2020-09-08 05:28:02 +0700

    #3 Lambda Python Code and template

--- deployment/lambda/release_index_generator/main.py
+++ deployment/lambda/release_index_generator/main.py
@@ -1,37 +1,83 @@
 from botocore.exceptions import ClientError
-from boto3 import client
+from boto3 import client, resource
 from os import path
 from jinja2 import Environment, FileSystemLoader
-from urllib.parse import unquote_plus
 
 s3_client = client('s3')
+s3_resource = resource('s3')
 env = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
 template = env.get_template('index.html')
 
 
-def generate_elements(bucket, folder_key):
-    print(folder_key)
-    response = s3_client.list_objects_v2(
-        Bucket=bucket,
-        Prefix=folder_key
+def format_files(files):
+    my_list = files
+    apps = dict()
+    for item in my_list:
+        if "/" not in item:
+            my_list.remove(item)
+    for item in my_list:
+        split = item.split("/")
+        apps.setdefault(split[0], set()).add(split[1])
+    for item in my_list:
+        split = item.split("/")
+        if len(split) <= 2:
+            my_list.remove(item)
+    return {"applications": apps, "files": my_list}
+
+
+def create_index(bucket_name, info):
+    element_main_li = ""
+    for app, versions in info['applications'].items():
+        element_app_li = "<li>\n  <a href=\"/" + "\"> ... </a>\n</li>"
+        for ver in versions:
+            element_ver_li = "<li>\n  <a href=\"/" + app + "/" + "\"> ... </a>\n</li>"
+            for file in info['files']:
+                if (file.find(app) >= 0) and (file.find(ver) >= 0):
+                    element_ver_li = element_ver_li + "\n<li>\n  <a href=\"/" + file + "\">" + file.rsplit("/", 1)[1] + "</a>\n</li>"
+            index_html_s3 = s3_resource.Object(bucket_name, app + "/" + ver + "/" + "index.html")
+            index_html_s3.put(
+                ACL='public-read',
+                Body=template.render(elements=element_ver_li),
+                ContentType='text/html'
+            )
+            element_app_li = element_app_li + "\n<li>\n  <a href=\"/" + app + "/" + ver + "\">" + app + "_" + ver + "</a>\n</li>"
+        index_html_s3 = s3_resource.Object(bucket_name, app + "/index.html")
+        index_html_s3.put(
+            ACL='public-read',
+            Body=template.render(elements=element_app_li),
+            ContentType='text/html'
+        )
+        element_main_li = element_main_li + "\n<li>\n  <a href=\"/" + app + "/" + "\">" + app + "</a>\n</li>"
+    index_html_s3 = s3_resource.Object(bucket_name, "index.html")
+    index_html_s3.put(
+        ACL='public-read',
+        Body=template.render(elements=element_main_li),
+        ContentType='text/html'
     )
-
-
-def resize_image(key, bucket):
-    tmpkey = key.replace('/', '')
-    download_path = '/tmp/{}{}'
-    upload_path = '/tmp/resized-{}'.format(tmpkey)
-    s3_client.download_file(bucket, key, download_path)
-    resize_image(download_path, upload_path)
-    s3_client.upload_file(upload_path, '{}-resized'.format(bucket), key)
+    return "OK"
 
 
 def lambda_handler(event, context):
-    print(event)
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        print(bucket)
-        print(record['s3']['object']['key'])
-        key = unquote_plus(record['s3']['object']['key'])
-        print(key)
-        generate_elements(bucket, key)
+    files = []
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    app = key.split("/")[0]
+    app_objs = s3_client.list_objects_v2(
+        Bucket=bucket,
+        Prefix=app
+    )
+    print("S3 objects:", app_objs)
+    if app_objs['KeyCount'] > 1:
+        for obj in app_objs['Contents']:
+            key = obj['Key']
+            if not key.endswith("index.html"):
+                if key.endswith("/"):
+                    files.append(key[:-1])
+                else:
+                    files.append(key)
+    files_info = format_files(sorted(files))
+    status = create_index(bucket, files_info)
+    return {
+        'statusCode': 200,
+        'body': status
+    }
